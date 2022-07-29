60bacf76b8b6d24a81864e1280b77f26865fbb04
mjjstockman <mjjstockman@gmail.com>
2022-07-29 11:17:23 +0000

    Add weclome(), validate name for empty or beggining with a number

--- run.py
+++ run.py
@@ -12,8 +12,58 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('game_shop')
 
-stock = SHEET.worksheet('stock')
 
-data = stock.get_all_values()
+def validate_data(values):
+    try:
+        if values == "":
+            raise ValueError("You must enter your name")
+        else: 
+            if values[0].isnumeric():
+                raise ValueError("The first character of your name cannot be a number")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
-print(data)
+
+
+def welcome():
+    print("Hi! Welcome to Game Shop!\n")
+    name = input("Please enter your name:\n").lower()
+    validate_data(name)
+    # print(f"\nHi, {name.capitalize()}! Here's the games we are currently selling...")
+
+
+
+
+welcome()
+
+
+
+    # while True:
+    #     if name is None:
+    #         name = input("Please enter your name:\n").lower()
+    #     elif confirm_data(name):
+    #         print(f"\nThanks {name.capitalize()}! Please choose from the following:\n")
+    #         break
+    #     else:
+    #         # print("\nInvalid choice, please try again\n")
+    #         continue
+
+
+
+# def confirm_data(data):
+#     print(f"You entered {data}\n")
+#     print("Is this correct?\n")
+#     confirm = input("Enter Y for yes, N for No:\n")
+#     confirm_strip_lcase = confirm.strip().lower()
+#     if confirm_strip_lcase == "y":
+#         print(f"You said {data} is correct!")
+#         return True
+#     elif confirm_strip_lcase == "n":
+#         print("Let's try again")
+#         return False
+#     else:
+#         print("Input must be either a Y or N")
+#         return False
+       
+
+# welcome()
